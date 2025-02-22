d048357688a5de3ae566590a5ee01e1a45990188
natriumkr <wookuki1004@gmail.com>
2025-02-22 02:31:55 +0000

    new file:   css/dice.css 	renamed:    css/style.css -> css/index.css 	new file:   html/dice.html 	modified:   index.html 	new file:   js/dice.js

---
+++ js/dice.js
@@ -0,0 +1,19 @@
+function dice(event){
+    event.preventDefault();
+    const max=max_input.value();
+    const number=number_input.value();
+    const additional=additional_input.value();
+    var dice=0;
+
+    for (i=0; i<number;i++){
+        var dice1=Math.floor(Math.random()*max);
+        dice=dice+dice1;
+    }
+    dice=dice+additional;
+    document.getElementById('output').innerHTML = dice;
+
+
+
+}
+
+button.addEventListener('click',dice);
